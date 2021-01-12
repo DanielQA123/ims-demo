17bf8c248092f2b3eae7befaae419e0a1ee02650
DanielQA123 <DAhuchogu@QA.com>
2021-01-12 22:07:24 +0000

    added resultset to itemDao

--- src/main/java/com/qa/ims/persistence/dao/ItemDaoMysql.java
+++ src/main/java/com/qa/ims/persistence/dao/ItemDaoMysql.java
@@ -1,6 +1,11 @@
 package com.qa.ims.persistence.dao;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import org.apache.log4j.Logger;
+
+import com.qa.ims.persistence.domain.Item;
 
 public class ItemDaoMysql implements Dao<Item> {
 	
@@ -10,6 +15,24 @@ public class ItemDaoMysql implements Dao<Item> {
 	private String username;
 	private String password;
 	
-	public ItemDaoMysql()
+	public ItemDaoMysql(String username, String password) {
+		this.jdbcConnectionUrl = "jdbc:mysql://localhost:3306/ims";
+		this.username = username;
+		this.password = password;
+	}
 	
+	public ItemDaoMysql(String jdbcConnectionUrl, String username, String password) {
+		this.jdbcConnectionUrl = jdbcConnectionUrl;
+		this.username = username;
+		this.password = password;
+	}
+	
+	Item itemFromResultSet(ResultSet resultSet) throws SQLException {
+	Long itemId = resultSet.getLong("id");
+	String itemName = resultSet.getString("item_name");
+	int itemQuantity = resultSet.getInt("quantity");
+	double price = resultSet.getDouble("price");
+	return new Item(itemId, itemName, itemQuantity, price);
+
+}
 }
