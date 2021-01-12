5572d74d872486e9bea114a2b8bc88c34c5b5a76
DanielQA123 <DAhuchogu@QA.com>
2021-01-12 21:52:28 +0000

    added itemDao implements

--- src/main/java/com/qa/ims/persistence/dao/ItemDaoMysql.java
+++ src/main/java/com/qa/ims/persistence/dao/ItemDaoMysql.java
@@ -1,5 +1,15 @@
 package com.qa.ims.persistence.dao;
 
-public class ItemDaoMysql implements Dao<Item> {
+import org.apache.log4j.Logger;
 
+public class ItemDaoMysql implements Dao<Item> {
+	
+	public static final Logger LOGGER = Logger.getLogger(ItemDaoMysql.class);
+    
+	private String jdbcConnectionUrl;
+	private String username;
+	private String password;
+	
+	public ItemDaoMysql()
+	
 }
