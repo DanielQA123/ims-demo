75aa376a896266f30c1182a239c9a7ef29696680
DanielQA123 <DAhuchogu@QA.com>
2021-01-12 22:22:13 +0000

    adding sql connection for itemDao

--- src/main/java/com/qa/ims/persistence/dao/ItemDaoMysql.java
+++ src/main/java/com/qa/ims/persistence/dao/ItemDaoMysql.java
@@ -1,10 +1,16 @@
 package com.qa.ims.persistence.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.log4j.Logger;
 
+import com.qa.ims.persistence.domain.Customer;
 import com.qa.ims.persistence.domain.Item;
 
 public class ItemDaoMysql implements Dao<Item> {
@@ -33,6 +39,49 @@ public class ItemDaoMysql implements Dao<Item> {
 	int itemQuantity = resultSet.getInt("quantity");
 	double price = resultSet.getDouble("price");
 	return new Item(itemId, itemName, itemQuantity, price);
+	}
 
+	//Reads all the items within the database 
+	//returns a list of items 
+	
+	@Override
+	public List<Item> readAll() {
+		try (Connection connection = DriverManager.getConnection(jdbcConnectionUrl, username, password);
+				Statement stat = connection.createStatement();
+				ResultSet resultSet = stat.executeQuery("select * from items");) {
+			ArrayList<Item> items = new ArrayList<>();
+			while (resultSet.next()) {
+			items.add(itemFromResultSet(resultSet));
+			}
+			return items;
+		} catch (SQLException e) {
+			LOGGER.debug(e.getStackTrace());
+			LOGGER.error(e.getMessage());
+		}
+		return new ArrayList<>();
+		
+		
+	}
+
+	@Override
+	public Item create(Item t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Item update(Item t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(long id) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
 }
-}
+
