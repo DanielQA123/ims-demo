48eeb2e3905b6066a9e7c406f424c67a8f91f87e
DanielQA123 <DAhuchogu@QA.com>
2021-01-12 21:22:48 +0000

    made another change to itemDomain

--- src/main/java/com/qa/ims/persistence/domain/Item.java
+++ src/main/java/com/qa/ims/persistence/domain/Item.java
@@ -2,44 +2,45 @@ package com.qa.ims.persistence.domain;
 
 public class Item {
 	// The products attributes
-	private Long ItemId;
-	private String ItemName;
-	private int ItemQuantity;
+	private Long itemId;
+	private String itemName;
+	private int itemQuantity;
 	private double price;
 	
-	public Item(String ItemName, int ItemQuantity, double price ) {
+	public Item(String itemName, int itemQuantity, double price ) {
 		super();
-		this.ItemName = ItemName;
-		this.ItemQuantity = ItemQuantity;
+		this.itemName = itemName;
+		this.itemQuantity = itemQuantity;
 		this.price = price;
 	}
-	public Item(Long ItemId, String ItemName, int ItemQuantity, double price) {
+	public Item(Long itemId, String itemName, int itemQuantity, double price) {
 		super();
-		this.ItemId = ItemId;
-		this.ItemName = ItemName;
-		this.ItemQuantity = ItemQuantity;
+		this.itemId = itemId;
+		this.itemName = itemName;
+		this.itemQuantity = itemQuantity;
 		this.price = price;
 	
 	//Getters and Setters 
 		
 	}
+	
 	public Long getItemId() {
-		return ItemId;
+		return itemId;
 	}
 	public void setItemId(Long itemId) {
-		ItemId = itemId;
+		this.itemId = itemId;
 	}
 	public String getItemName() {
-		return ItemName;
+		return itemName;
 	}
 	public void setItemName(String itemName) {
-		ItemName = itemName;
+		this.itemName = itemName;
 	}
 	public int getItemQuantity() {
-		return ItemQuantity;
+		return itemQuantity;
 	}
 	public void setItemQuantity(int itemQuantity) {
-		ItemQuantity = itemQuantity;
+		this.itemQuantity = itemQuantity;
 	}
 	public double getPrice() {
 		return price;
@@ -47,8 +48,21 @@ public class Item {
 	public void setPrice(double price) {
 		this.price = price;
 	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Customer other = (Customer) obj;
+		if (itemName == null) {
+			if (other.firstName != null)
+				return false;
 	
 
 	
 	
 }
+		}
