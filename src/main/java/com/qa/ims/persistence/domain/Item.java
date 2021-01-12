45f1fb40bbd1d3f930ec278a71e8b3c0039e2804
DanielQA123 <DAhuchogu@QA.com>
2021-01-12 21:02:10 +0000

    added my attributes in item

--- src/main/java/com/qa/ims/persistence/domain/Item.java
+++ src/main/java/com/qa/ims/persistence/domain/Item.java
@@ -1,5 +1,12 @@
 package com.qa.ims.persistence.domain;
 
 public class Item {
+	// The products attributes
+	private Long ItemId;
+	private String ItemName;
+	private Long ItemQuantity;
+	private double price;
+	
+	
 
 }
