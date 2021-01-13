6f055fc0da4508d09271a79145d0c270bde3b823
DanielQA123 <DAhuchogu@QA.com>
2021-01-13 10:13:53 +0000

    working on test itemController

--- src/test/java/com/qa/ims/controller/ItemControllerTest.java
+++ src/test/java/com/qa/ims/controller/ItemControllerTest.java
@@ -1,5 +1,25 @@
 package com.qa.ims.controller;
 
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+@RunWith(MockitoJUnitRunner.class)
+
 public class ItemControllerTest {
+
+  @Mock
+  private Dao<Item> ItemDao;
+  
+  @InjectMocks
+  private ItemServices itemServices;
+  
+  @Test
+  public void itemServicesCreate() {
+	  Item item = new Item();
+  }
+  
+  
+  
 
 }
