7c82c6266199eef0e93a51549fc2f4081c3428ca
DanielQA123 <DAhuchogu@QA.com>
2021-01-13 09:47:35 +0000

    starting creating a class for testing

---
+++ src/test/java/com/qa/ims/persistence/domain/ItemTest.java
@@ -0,0 +1,17 @@
+package com.qa.ims.persistence.domain;
+
+import org.junit.Before;
+
+public class ItemTest {
+	
+	private Item item;
+	private Item otherItem;
+	
+	@Before 
+	public void setUpTest() {
+		item = new Item();
+		
+		
+	}
+
+}
