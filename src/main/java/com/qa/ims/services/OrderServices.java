d33c4f23c7297ef8bf6c2f8404f14d3d29475d7c
DanielQA123 <DAhuchogu@QA.com>
2021-01-13 15:29:00 +0000

    making changes in orderServices

--- src/main/java/com/qa/ims/services/OrderServices.java
+++ src/main/java/com/qa/ims/services/OrderServices.java
@@ -1,5 +1,38 @@
 package com.qa.ims.services;
 
-public class OrderServices {
+import java.util.List;
+
+
+import com.qa.ims.persistence.dao.Dao;
+import com.qa.ims.persistence.domain.Order;
+
+public class OrderServices implements CrudServices<Order> {
+	
+	private Dao<Order> orderDao;
+	
+	public OrderServices(Dao<Order> orderDao) {
+		this.orderDao = orderDao;
+
+	}
+
+	public List<Order> readAll() {
+		return orderDao.readAll();	
+	}
+
+	public Order create(Order order) {
+		return orderDao.create(order);
+	}
+
+	@Override
+	public Order update(Order t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(Long id) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
