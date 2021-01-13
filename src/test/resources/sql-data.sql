713a0da8ea09092ace9bdca57a8750115fe39bd0
DanielQA123 <DAhuchogu@QA.com>
2021-01-13 11:07:21 +0000

    added info all my sql syntax

--- src/test/resources/sql-data.sql
+++ src/test/resources/sql-data.sql
@@ -3,3 +3,7 @@ insert into ims.customers(first_name, surname) values('rhys', 'thompson');
 insert into ims.customers(first_name, surname) values('nic', 'johnson');
 insert into ims.customers(first_name, surname) values('jordon', 'harrison');
 
+insert into ims.items(item_name, itemQuantity, price) values('Toaster','1','25.00');
+insert into ims.items(item_name, itemQuantity, price) values('Poker Set','1','35.00');
+insert into ims.items(item_name, itemQuantity, price) values('Laptop','2','325.00');
+insert into ims.items(item_name, itemQuantity, price) values('Toothpaste','3','3.00');
