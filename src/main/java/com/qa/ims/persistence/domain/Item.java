9971b34e1c6dc515e72e6b2ffd73080db363d7c8
DanielQA123 <DAhuchogu@QA.com>
2021-01-13 17:45:10 +0000

    Revisting item to get it working

--- src/main/java/com/qa/ims/persistence/domain/Item.java
+++ src/main/java/com/qa/ims/persistence/domain/Item.java
@@ -4,67 +4,66 @@ public class Item {
 	// The products attributes
 	private Long itemId;
 	private String itemName;
-	private int itemQuantity;
 	private double price;
-	
-	public Item(String itemName, int itemQuantity, double price ) {
+
+	public Item(String itemName, double price) {
 		super();
 		this.itemName = itemName;
-		this.itemQuantity = itemQuantity;
 		this.price = price;
 	}
-	public Item(Long itemId, String itemName, int itemQuantity, double price) {
+
+	public Item(Long itemId, String itemName, double price) {
 		super();
 		this.itemId = itemId;
 		this.itemName = itemName;
-		this.itemQuantity = itemQuantity;
 		this.price = price;
-	
-	//Getters and Setters 
-		
+
+		// Getters and Setters
+
 	}
-	
+
 	public Long getItemId() {
 		return itemId;
 	}
+
 	public void setItemId(Long itemId) {
 		this.itemId = itemId;
 	}
+
 	public String getItemName() {
 		return itemName;
 	}
+
 	public void setItemName(String itemName) {
 		this.itemName = itemName;
 	}
-	public int getItemQuantity() {
-		return itemQuantity;
-	}
-	public void setItemQuantity(int itemQuantity) {
-		this.itemQuantity = itemQuantity;
-	}
+
 	public double getPrice() {
 		return price;
 	}
+
 	public void setPrice(double price) {
 		this.price = price;
 	}
+
 	@Override
 	public String toString() {
-		return "Item [itemId=" + itemId + ", itemName=" + itemName + ", itemQuantity=" + itemQuantity + ", price="
-				+ price + "]";
-	}	
+		return "Item [itemId=" + itemId + ", itemName=" + itemName + ", price=" + price + "]";
+
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + ((itemId == null) ? 0 : itemId.hashCode());
 		result = prime * result + ((itemName == null) ? 0 : itemName.hashCode());
-		result = prime * result + itemQuantity;
 		long temp;
 		temp = Double.doubleToLongBits(price);
 		result = prime * result + (int) (temp ^ (temp >>> 32));
 		return result;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -84,12 +83,8 @@ public class Item {
 				return false;
 		} else if (!itemName.equals(other.itemName))
 			return false;
-		if (itemQuantity != other.itemQuantity)
-			return false;
 		if (Double.doubleToLongBits(price) != Double.doubleToLongBits(other.price))
 			return false;
 		return true;
 	}
-		
 }
-		
