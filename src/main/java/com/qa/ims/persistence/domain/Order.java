275f8907c9e277fbfd4ddc4547a6b8bda05389ad
DanielQA123 <DAhuchogu@QA.com>
2021-01-13 14:33:26 +0000

    working on order domain

--- src/main/java/com/qa/ims/persistence/domain/Order.java
+++ src/main/java/com/qa/ims/persistence/domain/Order.java
@@ -1,5 +1,24 @@
 package com.qa.ims.persistence.domain;
 
 public class Order {
+	private Long orderId;
+	private Long customerId;
+	private String shippingAddress;
+	
+	
+	public Order (String shippingAddress) {
+		super();
+		this.shippingAddress = shippingAddress;
+	}
+
+	public Order(Long orderId, Long customerId, String shippingAddress) {
+		super();
+		this.orderId = orderId;
+		this.customerId = customerId;
+		this.shippingAddress = shippingAddress;
+	}
+	
+	
+	
 
 }
