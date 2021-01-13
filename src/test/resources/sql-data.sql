88991e9d9948af57f4a951dc4c87873267795ef7
DanielQA123 <DAhuchogu@QA.com>
2021-01-13 18:10:18 +0000

    Added info to sqlschema

--- src/test/resources/sql-data.sql
+++ src/test/resources/sql-data.sql
@@ -3,3 +3,7 @@ insert into ims.customers(first_name, surname) values('rhys', 'thompson');
 insert into ims.customers(first_name, surname) values('nic', 'johnson');
 insert into ims.customers(first_name, surname) values('jordon', 'harrison');
 
+insert into ims.items(item_name, price) values('Toaster','24.00');
+insert into ims.items(item_name, price) values('Kettle','27.00');
+insert into ims.items(item_name, price) values('Laptop','444.00');
+insert into ims.items(item_name, price) values('iPad','250.00');
--- src/test/resources/sql-schema.sql
+++ src/test/resources/sql-schema.sql
@@ -1,3 +1,4 @@
 create database if not exists ims;
 drop table if exists ims.customers;
 create table ims.customers(id int primary key auto_increment, first_name varchar(40), surname varchar(40));
+create table if not exists ims.items(id int primary key auto_increment, item_name varchar(100), price decimal(6,2));
