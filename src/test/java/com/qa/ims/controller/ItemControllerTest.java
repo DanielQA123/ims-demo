9c32dcc1fe319d3e631991bbcc6b0f04ec094c2b
DanielQA123 <DAhuchogu@QA.com>
2021-01-13 10:25:43 +0000

    making changes to ItemControllerTest

--- src/test/java/com/qa/ims/controller/ItemControllerTest.java
+++ src/test/java/com/qa/ims/controller/ItemControllerTest.java
@@ -1,25 +1,42 @@
 package com.qa.ims.controller;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.Spy;
 import org.mockito.junit.MockitoJUnitRunner;
+
 @RunWith(MockitoJUnitRunner.class)
 
 public class ItemControllerTest {
 
   @Mock
-  private Dao<Item> ItemDao;
+  private ItemServices itemServices; 
   
+  @Spy
   @InjectMocks
-  private ItemServices itemServices;
+  private ItemController itemController;
   
   @Test
-  public void itemServicesCreate() {
-	  Item item = new Item();
+  public void readAllTest() {
+  }
+  
+  @Test
+  public void createTest() {
+	  String itemName = "";
+	  int itemQuantity = ;
+	  double price = ;
+	  Mockito.doReturn(itemName, price).when(itemController).getInput();
+	  
+	  
+  }
+  
+  
   }
   
   
   
 
-}
+
--- src/test/java/com/qa/ims/services/ItemServicesTest.java
+++ src/test/java/com/qa/ims/services/ItemServicesTest.java
@@ -1,5 +1,24 @@
 package com.qa.ims.services;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+
+@RunWith(MockitoJUnitRunner.class)
 public class ItemServicesTest {
 
+		  @Mock
+		  private Dao<Item> ItemDao;
+		  
+		  @InjectMocks
+		  private ItemServices itemServices;
+		  
+		  @Test
+		  public void itemServicesCreate() {
+			  Item item = new Item();
+			  itemServices.create(item);
+}
 }
