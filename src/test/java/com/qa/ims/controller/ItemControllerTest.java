4b88c6550b40f32d68422041719a021859098c92
DanielQA123 <DAhuchogu@QA.com>
2021-01-14 20:17:15 +0000

    working on itemControllerTest

--- src/test/java/com/qa/ims/controller/ItemControllerTest.java
+++ src/test/java/com/qa/ims/controller/ItemControllerTest.java
@@ -1,15 +1,21 @@
 package com.qa.ims.controller;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.Spy;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import com.qa.ims.persistence.domain.Item;
 import com.qa.ims.services.ItemServices;
-import com.sun.tools.javac.util.List;
+
 
 @RunWith(MockitoJUnitRunner.class)
 public class ItemControllerTest {
@@ -29,8 +35,16 @@ public class ItemControllerTest {
 	public void readAllTest() {
 		ItemController itemController = new ItemController(itemServices);
 		List<Item> items = new ArrayList<>();
-		items.add(new Item());
-		
+		items.add(new Item("Toaster",24.00));
+		Mockito.when(itemServices.readAll()).thenReturn(items);
+		assertEquals(items, itemController.readAll());
+	}
+	
+	@Test
+	public void createTest() {
+		String itemName ="Toaster";
+		double price = 24.00;
+		Mockito.doReturn(itemName, price).when(itemController).getInput();
 		
 	}
 
