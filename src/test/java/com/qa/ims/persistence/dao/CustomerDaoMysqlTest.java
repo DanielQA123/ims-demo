78a029a90fae11abdaab7bd06170999730febc4c
DanielQA123 <DAhuchogu@QA.com>
2021-01-16 17:02:58 +0000

    created dao test package

---
+++ src/test/java/com/qa/ims/persistence/dao/CustomerDaoMysqlTest.java
@@ -0,0 +1,13 @@
+package com.qa.ims.persistence.dao;
+
+import org.apache.log4j.Logger;
+
+public class CustomerDaoMysqlTest {
+
+	public static final Logger LOGGER = Logger.getLogger(CustomerDaoMysql.class);
+
+	private String jdbcConnectionUrl="";
+	private String username ="root";
+	private String password ="root";
+	
+}
