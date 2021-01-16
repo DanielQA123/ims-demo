4145c2b63bd9ff932b88938ab1953c632ec69106
DanielQA123 <DAhuchogu@QA.com>
2021-01-16 17:59:09 +0000

    setting up tests

--- src/test/java/com/qa/ims/persistence/dao/CustomerDaoMysqlTest.java
+++ src/test/java/com/qa/ims/persistence/dao/CustomerDaoMysqlTest.java
@@ -1,7 +1,13 @@
 package com.qa.ims.persistence.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
 import org.apache.log4j.Logger;
+import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Test;
 
 import com.qa.ims.Ims;
 
@@ -19,7 +25,23 @@ public class CustomerDaoMysqlTest {
 		
 		Ims ims = new Ims();
 		ims.init(jdbcConnectionUrl, username, password,"src/test/resources/sql-schema.sql");
-		
 	}
 	
+	@Before
+	public void setUp() {
+		try (Connection connection = DriverManager.getConnection(jdbcConnectionUrl, username, password);
+				Statement statement = connection.createStatement();) {
+			statement.executeUpdate("delete from customers;");
+		} catch (Exception e) {
+			LOGGER.debug(e.getStackTrace());
+			LOGGER.error(e.getMessage());
+		}
+	}
+	
+	@Test
+	public void createTest() {
+		CustomerDaoMysql customerDaoMysql = new CustomerDaoMysql(jdbcConnectionUrl, username, password);
+		
+		
+	}
 }
