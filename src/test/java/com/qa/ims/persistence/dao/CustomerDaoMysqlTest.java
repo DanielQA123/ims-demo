9bc5b7502b8614bf4100b03efa2414964a734333
DanielQA123 <DAhuchogu@QA.com>
2021-01-16 17:12:31 +0000

    intialzing my ims test cust dao

--- src/test/java/com/qa/ims/persistence/dao/CustomerDaoMysqlTest.java
+++ src/test/java/com/qa/ims/persistence/dao/CustomerDaoMysqlTest.java
@@ -1,13 +1,25 @@
 package com.qa.ims.persistence.dao;
 
 import org.apache.log4j.Logger;
+import org.junit.BeforeClass;
+
+import com.qa.ims.Ims;
 
 public class CustomerDaoMysqlTest {
 
 	public static final Logger LOGGER = Logger.getLogger(CustomerDaoMysql.class);
 
-	private String jdbcConnectionUrl="";
-	private String username ="root";
-	private String password ="root";
+	private static String jdbcConnectionUrl="jdbc:mysql://localhost:3306/ims_test";
+	private static String username ="root";
+	private static String password ="root";
+	
+	@BeforeClass
+	public static void init() {
+	//intialize the ims object 
+		
+		Ims ims = new Ims();
+		ims.init(jdbcConnectionUrl, username, password,"src/test/resources/sql-schema.sql");
+		
+	}
 	
 }
