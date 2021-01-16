68ead726e83389a7f6e95cc39e2d1184f977ef50
DanielQA123 <DAhuchogu@QA.com>
2021-01-16 15:39:04 +0000

    creating orderline domain

--- src/main/java/com/qa/ims/persistence/dao/OrderlineDaoMysql.java
+++ src/main/java/com/qa/ims/persistence/dao/OrderlineDaoMysql.java
@@ -1,5 +1,11 @@
 package com.qa.ims.persistence.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.log4j.Logger;
@@ -14,13 +20,45 @@ public class OrderlineDaoMysql implements Dao<Order> {
 	private String username;
 	private String password;
 	
+	public OrderlineDaoMysql(String username, String password ) {
+		this.jdbcConnectionUrl = "jdbc:mysql://localhost:3306/ims";
+		this.username = username;
+		this.password = password;
+	}
+	public OrderlineDaoMysql(String jdbcConnectionUrl, String username, String password) {
+		super();
+		this.jdbcConnectionUrl = jdbcConnectionUrl;
+		this.username = username;
+		this.password = password;
+	}
 	
-	
+	Order orderlineFromResultSet(ResultSet resultSet) throws SQLException {
+		Long orderId = resultSet.getLong("id");
+		String shippingAddress = resultSet.getString("shippingAddress");
+		Long customerId = resultSet.getLong("customer_id");
+		String itemName = resultSet.getString("item_name");
+		return new Order(orderId, shippingAddress, customerId, itemName);
+	}
+
 	@Override
 	public List<Order> readAll() {
-		// TODO Auto-generated method stub
-		return null;
+		try (Connection connection = DriverManager.getConnection(jdbcConnectionUrl, username, password);
+				Statement stat = connection.createStatement();
+				ResultSet resultSet = stat.executeQuery("SELECT * from orderline");) {
+			ArrayList<Order> orderlines = new ArrayList<>();
+			while (resultSet.next()) {
+				orderlines.add(orderlineFromResultSet(resultSet));
+			}
+			return orderlines;
+		} catch (SQLException e) {
+			LOGGER.debug(e.getStackTrace());
+			LOGGER.error(e.getMessage());
+		}
+		return new ArrayList<>();
 	}
+	
+	public Order 
+	
 
 	@Override
 	public Order create(Order t) {
---
+++ src/main/java/com/qa/ims/persistence/domain/Orderline.java
@@ -0,0 +1,12 @@
+package com.qa.ims.persistence.domain;
+
+public class Orderline {
+	
+	private Long orderlineId;
+	private Long orderId;
+	private String shippingAddress;
+	private String itemName;
+	
+	
+
+}
