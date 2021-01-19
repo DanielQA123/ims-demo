63bd8f242c5c6a247fb611e3b05f5ee19bffc4d1
DanielQA123 <DAhuchogu@QA.com>
2021-01-19 22:17:44 +0000

    order controller test

---
+++ src/test/java/com/qa/ims/controller/OrderControllerTest.java
@@ -0,0 +1,23 @@
+package com.qa.ims.controller;
+
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import com.qa.ims.services.OrderServices;
+
+@RunWith(MockitoJUnitRunner.class)
+public class OrderControllerTest {
+	
+	@Mock
+	private OrderServices orderServices;
+	
+	@Spy
+	@InjectMocks
+	private OrderController orderController;
+	
+	
+
+}
