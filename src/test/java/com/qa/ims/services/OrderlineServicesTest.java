4bd3db88134329323e60aef1034a77eea427d1bf
DanielQA123 <DAhuchogu@QA.com>
2021-01-20 12:36:16 +0000

    orderline Services test

--- src/test/java/com/qa/ims/services/OrderlineServicesTest.java
+++ src/test/java/com/qa/ims/services/OrderlineServicesTest.java
@@ -1,10 +1,50 @@
 package com.qa.ims.services;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
+
+import com.qa.ims.persistence.dao.Dao;
+import com.qa.ims.persistence.domain.Orderline;
 
 @RunWith(MockitoJUnitRunner.class)
 public class OrderlineServicesTest {
+	
+	@Mock
+	private Dao<Orderline> orderlineDao;
+	
+	@InjectMocks
+	private OrderlineServices orderlineServices;
+	
+	@Test
+	public void orderlineServicesCreate() {
+		Orderline orderline = new Orderline(1L, 1L, 1L, 1L);
+		orderlineServices.create(orderline);
+		Mockito.verify(orderlineDao, Mockito.times(1)).create(orderline);
+	}
+	
+	@Test
+	public void orderlineServicesRead() {
+		orderlineServices.readAll();
+		Mockito.verify(orderlineDao, Mockito.times(1)).readAll();
+	}
+	
+	@Test
+	public void orderlineServicesUpdate() {
+		Orderline orderline = new Orderline(1L,1L,1L,1L);
+		orderlineServices.update(orderline);
+		Mockito.verify(orderlineDao, Mockito.times(1)).update(orderline);
+	}
+	
+	@Test
+	public void orderlineServicesDelete() {
+		orderlineServices.delete(1L);
+		Mockito.verify(orderlineDao, Mockito.times(1)).delete(1L);
+	}
+	
 	
 
 }
