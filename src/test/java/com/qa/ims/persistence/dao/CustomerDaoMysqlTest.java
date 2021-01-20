4751f5b016950f3ca36eddd6e4fdbd33f4802fb2
DanielQA123 <DAhuchogu@QA.com>
2021-01-20 16:19:55 +0000

    customer dao test

---
+++ src/test/java/com/qa/ims/persistence/dao/CustomerDaoMysqlTest.java
@@ -0,0 +1,18 @@
+package com.qa.ims.persistence.dao;
+
+import org.apache.log4j.Logger;
+import org.junit.Before;
+
+public class CustomerDaoMysqlTest {
+
+	public static final Logger LOGGER = Logger.getLogger(CustomerDaoMysql.class);
+
+	private String jdbcConnectionUrl = "jdbc:mysql://localhost:3306/ims_test";
+	private String username = "root";
+	private String password = "root";
+	
+	@Before
+	public static void 
+	
+	
+}
