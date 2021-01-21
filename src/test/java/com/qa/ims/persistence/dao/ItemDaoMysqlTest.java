32679b8e301b379938071d74eae3acb8d6caa2e9
DanielQA123 <DAhuchogu@QA.com>
2021-01-21 00:03:36 +0000

    coverage at 72.2%

--- src/test/java/com/qa/ims/persistence/dao/ItemDaoMysqlTest.java
+++ src/test/java/com/qa/ims/persistence/dao/ItemDaoMysqlTest.java
@@ -1,8 +1,12 @@
 package com.qa.ims.persistence.dao;
 
+import static org.junit.Assert.assertEquals;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.log4j.Logger;
 import org.junit.Before;
@@ -13,9 +17,9 @@ import com.qa.ims.Ims;
 import com.qa.ims.persistence.domain.Item;
 
 public class ItemDaoMysqlTest {
-	
-public static final Logger LOGGER = Logger.getLogger(ItemDaoMysql.class);
-    
+
+	public static final Logger LOGGER = Logger.getLogger(ItemDaoMysql.class);
+
 	private static String jdbcConnectionUrl = "jdbc:mysql://localhost:3306/ims_test";
 	private static String username = "root";
 	private static String password = "root";
@@ -25,19 +29,19 @@ public static final Logger LOGGER = Logger.getLogger(ItemDaoMysql.class);
 		Ims ims = new Ims();
 		ims.init(jdbcConnectionUrl, username, password, "src/test/resources/sql-schema.sql");
 	}
-	
+
 	@Before
 	public void setUp() {
 		try (Connection connection = DriverManager.getConnection(jdbcConnectionUrl, username, password);
 				Statement stat = connection.createStatement();) {
 			stat.executeUpdate("DELETE from items;");
-			
+
 		} catch (Exception e) {
 			LOGGER.debug(e.getStackTrace());
 			LOGGER.error(e.getMessage());
 		}
 	}
-	
+
 	@Test
 	public void createTest() {
 		ItemDaoMysql itemDaoMysql = new ItemDaoMysql(jdbcConnectionUrl, username, password);
@@ -45,6 +49,48 @@ public static final Logger LOGGER = Logger.getLogger(ItemDaoMysql.class);
 		String itemName = "Toaster";
 		double price = 24.00;
 		Item item = new Item(1L, itemName, price);
-		
+		Item savedItem = new Item(1L, itemName, price);
+		item = itemDaoMysql.create(item);
+		assertEquals(savedItem, item);
 	}
-}
+
+	@Test
+	public void readTest() {
+		ItemDaoMysql itemDaoMysql = new ItemDaoMysql(jdbcConnectionUrl, username, password);
+		List<Item> items = new ArrayList<>();
+		Long itemId = 1L;
+		String itemName = "Toaster";
+		double price = 24.00;
+		Item item = new Item(1L, itemName, price);
+		Item savedItem = new Item(1L, itemName, price);
+		items = itemDaoMysql.readAll();
+		assertEquals(savedItem, item);
+	}
+
+	@Test
+	public void readAllTest() {
+		List<Item> items = new ArrayList<>();
+		ItemDaoMysql itemDaoMysql = new ItemDaoMysql(jdbcConnectionUrl, username, password);
+		Long itemId = 1L;
+		String itemName = "Toaster";
+		double price = 24.00;
+		Item item = new Item(1L, itemName, price);
+		Item savedItem = new Item(1L, itemName, price);
+		items = itemDaoMysql.readAll();
+		assertEquals(savedItem, item);
+	}
+	
+	@Test
+	public void updateTest() {
+		ItemDaoMysql itemDaoMysql = new ItemDaoMysql(jdbcConnectionUrl, username, password);
+		Long itemId = 1L;
+		String itemName = "Toaster";
+		double price = 24.00;
+		Item item = new Item(1L, itemName, price);
+		Item savedItem = new Item(1L, itemName, price);
+		item = itemDaoMysql.update(savedItem);
+		assertEquals(savedItem, item);
+	}
+
+	}
+
